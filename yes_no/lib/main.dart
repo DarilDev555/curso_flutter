55f786ed82a2ca51d163530dd6fad476fc948ef3
Hernandez Jimenez Daril Andrey <oneandrey555@outlook.com>
2025-01-09 12:21:38 -0600

    yes_no

---
+++ yes_no/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no/config/theme/app_theme.dart';
+import 'package:yes_no/presentation/chat/chat_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme( selectedColor: 0 ).theme(),
+      home:const ChatScreen()
+    );
+  }
+}
